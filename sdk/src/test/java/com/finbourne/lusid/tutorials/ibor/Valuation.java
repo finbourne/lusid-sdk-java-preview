e68eb5068e0d7c4571369cf7bd4a7aa5ea09f3e2
Khalid Keenan <khalid.keenan@finbourne.com>
2020-05-22 18:04:05 +0100

    DEV-8137 Add multi-currency transactions aggregation tutorial
    - Add an example in the Valuation tutorial of a portfolio in
    a base currency having transactions applied in instruments of
    different currencies

--- sdk/src/test/java/com/finbourne/lusid/tutorials/ibor/Valuation.java
+++ sdk/src/test/java/com/finbourne/lusid/tutorials/ibor/Valuation.java
@@ -11,25 +11,29 @@ import com.finbourne.lusid.utilities.TestDataUtilities;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.text.DecimalFormat;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.finbourne.lusid.utilities.TestDataUtilities.TutorialScope;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toMap;
 import static org.junit.Assert.assertEquals;
 
 public class Valuation {
 
     private static final String AGGREGATION_KEY = "Holding/default/PV";
     private static final String AGGREGATION_RESULT_KEY = "Sum(Holding/default/PV)";
-    private static final String GROUPBY_KEY = "Instrument/default/Name";
+    private static final String GROUPBY_INSTRUMENT_NAME_KEY = "Instrument/default/Name";
+    private static final String GROUPBY_PORTFOLIO_NAME_KEY = "Portfolio/default/Name";
 
     private final OffsetDateTime EFFECTIVE_DATE = OffsetDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC);
+    private final DecimalFormat df3 = new DecimalFormat("#.###");
 
     private static TransactionPortfoliosApi transactionPortfoliosApi;
     private static QuotesApi quotesApi;
@@ -83,6 +87,86 @@ public class Valuation {
                 });
     }
 
+    @Test
+    public void run_aggregation_on_multicurrency_portfolio_with_stock_in() throws ApiException {
+        //  setup test environment scope
+        String uuid = UUID.randomUUID().toString();
+        String quotesScope = uuid;
+
+        // begin test data preparation...
+
+        //  prepare portfolio in baseline GBP currency
+        String portfolioId = String.format("Id-%s", uuid);
+        String portfolioDisplayName = String.format("Portfolio-%s", uuid);
+        CreateTransactionPortfolioRequest transactionPortfolioRequest = createTransactionPortfolioRequest(portfolioId, portfolioDisplayName, "GBP");
+
+        //  prepare transactions  across GBP and USD. transactions are cash funding and stock fundings
+        List<TransactionRequest> transactionRequests = new ArrayList<>();
+        transactionRequests.add(testDataUtilities.buildCashFundsInTransactionRequest(10000.0, "GBP", EFFECTIVE_DATE));
+        transactionRequests.add(testDataUtilities.buildTransactionRequest(instrumentIds.get(0), 100.0, 100.0, "USD", EFFECTIVE_DATE, "StockIn"));
+        transactionRequests.add(testDataUtilities.buildTransactionRequest(instrumentIds.get(1), 100.0, 100.0, "GBP", EFFECTIVE_DATE, "StockIn"));
+        transactionRequests.add(testDataUtilities.buildTransactionRequest(instrumentIds.get(2), 100.0, 100.0, "GBP", EFFECTIVE_DATE, "StockIn"));
+
+        //  create and upload the instrument quotes in their respective currencies
+        List<UpsertQuoteRequest> quotes = new ArrayList<>();
+        // prepare market data quotes
+        quotes.addAll(asList(
+                createMarketDataUpsertQuoteRequest(instrumentIds.get(0), "USD", 200.00),
+                createMarketDataUpsertQuoteRequest(instrumentIds.get(1), "GBP", 200.00),
+                createMarketDataUpsertQuoteRequest(instrumentIds.get(2), "GBP", 80.00)
+        ));
+        // prepare fx data quotes
+        quotes.addAll(asList(
+                createFxUpsertQuoteRequest("GBP", "USD", 1.22),
+                createFxUpsertQuoteRequest("USD", "GBP", 0.68)
+        ));
+        // package quotes by quote ids
+        Map<String, UpsertQuoteRequest> quotesById = quotes.stream()
+                .collect(toMap(q -> UUID.randomUUID().toString(), Function.identity()));
+
+        // end test data preparation
+
+        // begin executing LUSID calls...
+
+        // create and validate portfolio creation
+        Portfolio portfolio = transactionPortfoliosApi.createPortfolio(TutorialScope, transactionPortfolioRequest);
+        assertEquals(portfolio.getId().getCode(), portfolioId);
+
+        // upload the transactions
+        transactionPortfoliosApi.upsertTransactions(TutorialScope, portfolioId, transactionRequests);
+
+        // upload the latest market quotes
+        quotesApi.upsertQuotes(quotesScope, quotesById);
+
+        //  run aggregations at the instrument and portfolio levels
+        List<Map<String, Object>> aggregationResultsByInstrument = runAggregation(portfolioId, quotesScope, GROUPBY_INSTRUMENT_NAME_KEY);
+        List<Map<String, Object>> aggregationResultsByPortfolio = runAggregation(portfolioId, quotesScope, GROUPBY_PORTFOLIO_NAME_KEY);
+
+        // end executing LUSID calls...
+
+
+        // begin verification of aggregation results...
+
+        // verify instruments aggregated in the base portfolio currency (GBP)
+        assertEquals("ASTRAZENECA PLC should have increased in value and been converted to GBP for the aggregation",
+                13600.0, aggregationResultsByInstrument.get(0).get(AGGREGATION_RESULT_KEY));
+        assertEquals("CENTRICA PLC should have increased in value base on market quote movement",
+                20000.0, aggregationResultsByInstrument.get(1).get(AGGREGATION_RESULT_KEY));
+        assertEquals("DIAEGO PLC should have decreased in value base on market quote movement",
+                8000.0, aggregationResultsByInstrument.get(2).get(AGGREGATION_RESULT_KEY));
+        assertEquals("Cash position should remain unchanged",
+                10000.0, aggregationResultsByInstrument.get(3).get(AGGREGATION_RESULT_KEY));
+
+        // verify portfolio aggregated in the base portfolio currency (GBP)
+        assertEquals(51600.0, aggregationResultsByPortfolio.get(0).get(AGGREGATION_RESULT_KEY));
+
+        // verify instruments add up to the portfolio level aggregation in the base currency
+        double totalInstrumentValueInBaseCcy = aggregationResultsByInstrument.stream()
+                .mapToDouble(i -> (double) i.get(AGGREGATION_RESULT_KEY))
+                .sum();
+        assertEquals(totalInstrumentValueInBaseCcy, aggregationResultsByPortfolio.get(0).get(AGGREGATION_RESULT_KEY));
+    }
+
     private void run_aggregation(
             Supplier<List<TransactionRequest>> createTransactionRequests,
             Consumer<List<Map<String, Object>>> validateResults
@@ -131,7 +215,7 @@ public class Valuation {
                         )
                         .metricValue(new MetricValue().value(x.getValue()).unit("GBP"))
                 )
-                .collect(Collectors.toMap(x -> UUID.randomUUID().toString(), Function.identity()));
+                .collect(toMap(x -> UUID.randomUUID().toString(), Function.identity()));
 
         //  upload the quotes
         quotesApi.upsertQuotes(quotesScope, quotes);
@@ -151,20 +235,20 @@ public class Valuation {
         //    create the aggregation request, this example calculates the percentage of total portfolio value and value by instrument
         AggregationRequest  aggregationRequest = new AggregationRequest()
                 .inlineRecipe(configurationRecipe)
-                .metrics(Arrays.asList(
-                        new AggregateSpec().key(GROUPBY_KEY).op(AggregateSpec.OpEnum.VALUE),
+                .metrics(asList(
+                        new AggregateSpec().key(GROUPBY_INSTRUMENT_NAME_KEY).op(AggregateSpec.OpEnum.VALUE),
                         new AggregateSpec().key(AGGREGATION_KEY).op(AggregateSpec.OpEnum.PROPORTION),
                         new AggregateSpec().key(AGGREGATION_KEY).op(AggregateSpec.OpEnum.SUM)
                 ))
-                .groupBy(Collections.singletonList(GROUPBY_KEY))
+                .groupBy(Collections.singletonList(GROUPBY_INSTRUMENT_NAME_KEY))
                 .effectiveAt(EFFECTIVE_DATE.toString());
 
         //  do the aggregation
         ListAggregationResponse aggregationResponse = aggregationApi.getAggregationByPortfolio(TutorialScope, portfolioId,null, null, null, aggregationRequest);
 
         aggregationResponse.getData().sort((o1, o2) -> {
-            String name1 = (String)o1.get(GROUPBY_KEY);
-            String name2 = (String)o2.get(GROUPBY_KEY);
+            String name1 = (String)o1.get(GROUPBY_INSTRUMENT_NAME_KEY);
+            String name2 = (String)o2.get(GROUPBY_INSTRUMENT_NAME_KEY);
 
             return name1.compareTo(name2);
         });
@@ -178,8 +262,136 @@ public class Valuation {
         ResultDataSchema    resultSchema = aggregationResponse.getDataSchema();
 
         for (Map<String, Object> aggregation : aggregationResponse.getData()) {
-            System.out.println(aggregation.get(GROUPBY_KEY) + ":\t" + aggregation.get(AGGREGATION_RESULT_KEY) + "\t");
+            System.out.println(aggregation.get(GROUPBY_INSTRUMENT_NAME_KEY) + ":\t" + aggregation.get(AGGREGATION_RESULT_KEY) + "\t");
         }
+    }
+
+    /**
+     * Run an aggregation on LUSID for a given portfolio and scope to produce values in the portfolio
+     * base currency. The aggregation level is defined by the grouping key.
+     *
+     * @param portfolioId - the portfolio to execute the aggregation on
+     * @param quotesScope - scope the aggregation retrieves quotes from
+     * @param groupingKey - grouping level of the aggregation
+     * @return
+     * @throws ApiException
+     */
+    private List<Map<String, Object>> runAggregation(String portfolioId, String quotesScope, String groupingKey) throws ApiException {
+        //  create the recipe that will instruct the aggregation on the market data provider and scope.
+        ConfigurationRecipe configurationRecipe = new ConfigurationRecipe()
+                .code("quotes_recipe")
+                .market(new MarketContext()
+                        .suppliers(new MarketContextSuppliers().equity(MarketContextSuppliers.EquityEnum.DATASCOPE))
+                        .options(new MarketOptions()
+                                .defaultSupplier(MarketOptions.DefaultSupplierEnum.DATASCOPE)
+                                .defaultInstrumentCodeType(MarketOptions.DefaultInstrumentCodeTypeEnum.LUSIDINSTRUMENTID)
+                                .defaultScope(quotesScope)
+                        )
+                );
+
+        //  create the aggregation request, this example calculates the percentage of total portfolio value and value by instrument
+        AggregationRequest  aggregationRequest = new AggregationRequest()
+                .inlineRecipe(configurationRecipe)
+                .metrics(asList(
+                        new AggregateSpec().key(groupingKey).op(AggregateSpec.OpEnum.VALUE),
+                        new AggregateSpec().key(AGGREGATION_KEY).op(AggregateSpec.OpEnum.PROPORTION),
+                        new AggregateSpec().key(AGGREGATION_KEY).op(AggregateSpec.OpEnum.SUM)
+                ))
+                .groupBy(Collections.singletonList(groupingKey))
+                .effectiveAt(EFFECTIVE_DATE.toString());
+
+        //  do the aggregation
+        ListAggregationResponse aggregationResponse = aggregationApi.getAggregationByPortfolio(TutorialScope, portfolioId,null, null, null, aggregationRequest);
+
+        aggregationResponse.getData().sort((o1, o2) -> {
+            String name1 = (String)o1.get(groupingKey);
+            String name2 = (String)o2.get(groupingKey);
+
+            return name1.compareTo(name2);
+        });
+
+        List<Map<String, Object>> aggregationResults = aggregationResponse.getData();
+        logResults(groupingKey, aggregationResults);
+        return  aggregationResults;
+    }
+
+
+    /**
+     * Create a request to submit to LUSID to generate a transaction portfolio.
+     *
+     * @param portfolioId - unique portfolio id
+     * @param displayName - display name of the portfolio
+     * @param currency - base currency of the portfolio
+     * @return
+     * @throws ApiException
+     */
+    private CreateTransactionPortfolioRequest createTransactionPortfolioRequest(String portfolioId, String displayName, String currency) throws ApiException {
+        //  build the create portfolio request
+        CreateTransactionPortfolioRequest request = new CreateTransactionPortfolioRequest()
+                .displayName(displayName)
+                .code(portfolioId)
+                .baseCurrency(currency)
+                .created(EFFECTIVE_DATE);
+
+        return request;
+    }
+
+    /**
+     * Creates a market data entry quote request.
+     *
+     * @param instrument - market data instrument
+     * @param currency - base currency of the instrument
+     * @param value - value of the quote
+     * @return
+     */
+    private UpsertQuoteRequest createMarketDataUpsertQuoteRequest(String instrument, String currency, double value){
+        return new UpsertQuoteRequest()
+                .quoteId(new QuoteId()
+                        .quoteSeriesId(new QuoteSeriesId()
+                                .provider("DataScope")
+                                .instrumentId(instrument)
+                                .instrumentIdType(QuoteSeriesId.InstrumentIdTypeEnum.LUSIDINSTRUMENTID)
+                                .quoteType(QuoteSeriesId.QuoteTypeEnum.PRICE)
+                                .field("mid")
+                        )
+                        .effectiveAt(EFFECTIVE_DATE.toString())
+                )
+                .metricValue(new MetricValue().value(value).unit(currency));
+    }
+
+    /**
+     * Creates an fx entry quote request.
+     *
+     * @param baseCcy - base currency of the quote
+     * @param counterCcy - counter currency of the quote
+     * @param rate - the exchange rate
+     * @return
+     */
+    private UpsertQuoteRequest createFxUpsertQuoteRequest(String baseCcy, String counterCcy, double rate){
+        return new UpsertQuoteRequest()
+                .quoteId(new QuoteId()
+                        .quoteSeriesId(new QuoteSeriesId()
+                                .provider("DataScope")
+                                .instrumentId(baseCcy + "/" + counterCcy)
+                                .instrumentIdType(QuoteSeriesId.InstrumentIdTypeEnum.CURRENCYPAIR)
+                                .quoteType(QuoteSeriesId.QuoteTypeEnum.PRICE)
+                                .field("mid")
+                        )
+                        .effectiveAt(EFFECTIVE_DATE.toString())
+                )
+                .metricValue(new MetricValue().value(rate).unit(baseCcy));
+    }
+
+    /**
+     *  Output aggregation results to console.
+     */
+    private void logResults(String resultSetName, List<Map<String, Object>> results){
+        System.out.println("---- Aggregation Results On Grouping By " + resultSetName + " ----");
+        for (Map<String, Object> aggregation : results) {
+            System.out.println(aggregation.get(resultSetName) + ":\t" + aggregation.get(AGGREGATION_RESULT_KEY) + "\t" +
+                    "(" + df3.format(aggregation.get("Proportion(Holding/default/PV)")) +")");
+        }
+        System.out.println("...");
     }
 
 }
