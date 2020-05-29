c40a6ee4af85a255f04b6d2469c04cfe46af7e57
Khalid Keenan <khalid.keenan@finbourne.com>
2020-05-29 13:54:56 +0100

    DEV-8132 : Adding Lusid API Factory
    - LusidApiFactory generates LUSID API objects based on a given
      configuration file. Factory is a utility method for cleaner
      instantiation.
    
    - Adding Mockito test dependencies

---
+++ sdk/src/main/java/com/finbourne/lusid/utilities/LusidApiFactoryBuilder.java
@@ -0,0 +1,49 @@
+package com.finbourne.lusid.utilities;
+
+import com.finbourne.lusid.ApiClient;
+
+import java.io.IOException;
+
+public class LusidApiFactoryBuilder {
+
+    /**
+     * Build a {@link LusidApiFactory} defining configuration using environment variables. For details on the environment arguments see https://support.lusid.com/getting-started-with-apis-sdks.
+     *
+     * @return
+     */
+    public static LusidApiFactory build() throws IOException {
+        if (!areRequiredEnvironmentVariablesSet()) {
+            throw new IllegalStateException("Environment variables to configure LUSID API client have not been set. See " +
+                    " see https://support.lusid.com/getting-started-with-apis-sdks for details.");
+        }
+        return createLusidApiFactory("");
+    }
+
+    /**
+     * Build a {@link LusidApiFactory} using the specified configuration file. For details on the format of the configuration file see https://support.lusid.com/getting-started-with-apis-sdks.
+     */
+    public static LusidApiFactory build(String configurationFile) throws IOException {
+        return createLusidApiFactory(configurationFile);
+    }
+
+    /**
+     * Build a {@link LusidApiFactory} using constant personal access tokens is not currently supported in the lusid java sdk
+     */
+    public static LusidApiFactory build(Object accessToken){
+        throw new UnsupportedOperationException("Connecting to LUSID via flat access tokens is not yet supported in the LUSID java sdk.");
+    }
+
+    private static LusidApiFactory createLusidApiFactory(String configurationFile) throws IOException {
+        ApiClient apiClient = new ApiClientBuilder().build(configurationFile);
+        return new LusidApiFactory(apiClient);
+    }
+
+    private static boolean areRequiredEnvironmentVariablesSet(){
+        return (System.getenv("FBN_TOKEN_URL") != null &&
+                System.getenv("FBN_USERNAME") != null &&
+                System.getenv("FBN_PASSWORD") != null &&
+                System.getenv("FBN_CLIENT_ID") != null &&
+                System.getenv("FBN_CLIENT_SECRET") != null &&
+                System.getenv("FBN_LUSID_API_URL") != null);
+    }
+}
