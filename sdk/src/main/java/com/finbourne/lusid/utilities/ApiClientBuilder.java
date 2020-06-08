d85c77f40a57bf9774523b2de3606c6c28523d3f
Rizwan Saeed <rizwansaeed@users.noreply.github.com>
2020-06-08 11:49:44 +0100

    Revert "DEV-8132 : ApiClient Factory : Refreshing Token"
    This reverts commit 6166b2c564793b46b989d0b2e08423463e666885.

--- sdk/src/main/java/com/finbourne/lusid/utilities/ApiClientBuilder.java
+++ sdk/src/main/java/com/finbourne/lusid/utilities/ApiClientBuilder.java
@@ -1,56 +1,91 @@
 package com.finbourne.lusid.utilities;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.finbourne.lusid.ApiClient;
-import com.finbourne.lusid.utilities.auth.HttpLusidTokenProvider;
-import com.finbourne.lusid.utilities.auth.KeepAuthTokenProvider;
-import com.finbourne.lusid.utilities.auth.LusidToken;
-import okhttp3.OkHttpClient;
+import okhttp3.*;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 /**
  * Utility class to build an ApiClient from a set of configuration
  */
 public class ApiClientBuilder {
 
-    public ApiClient build(String apiSecretsFilename){
-        // setup configuration from secrets file
-        ApiConfiguration apiConfiguration = createApiConfiguration(apiSecretsFilename);
-        // http client to use for api and auth calls
-        OkHttpClient httpClient = createHttpClient(apiConfiguration);
+    private static final MediaType FORM = MediaType.parse("application/x-www-form-urlencoded");
 
-        // token provider to keep client authenticated with automated token refreshing
-        KeepAuthTokenProvider keepAuthTokenProvider = new KeepAuthTokenProvider(new HttpLusidTokenProvider(apiConfiguration, httpClient));
-        LusidToken lusidToken = keepAuthTokenProvider.get();
+    public ApiClient build(String apiSecretsFilename) throws IOException
+    {
+        ApiConfiguration    apiConfiguration = new ApiConfigurationBuilder().build(apiSecretsFilename);
 
-        // setup api client that managed submissions with latest token
-        ApiClient defaultApiClient = createDefaultApiClient(apiConfiguration, httpClient, lusidToken);
-        return new KeepLiveApiClient(defaultApiClient, keepAuthTokenProvider);
-    }
+        //  request body
+        final String    tokenRequestBody = String.format("grant_type=password&username=%s&password=%s&scope=openid client groups&client_id=%s&client_secret=%s",
+                apiConfiguration.getUsername(),
+                URLEncoder.encode(apiConfiguration.getPassword(), StandardCharsets.UTF_8.toString()),
+                apiConfiguration.getClientId(),
+                URLEncoder.encode(apiConfiguration.getClientSecret(), StandardCharsets.UTF_8.toString()));
 
-    private ApiClient createDefaultApiClient(ApiConfiguration apiConfiguration, OkHttpClient httpClient, LusidToken lusidToken){
-        ApiClient apiClient = new ApiClient();
+        final OkHttpClient    httpClient;
+
+        //  use a proxy if given
+        if (apiConfiguration.getProxyAddress() != null) {
+
+            InetSocketAddress proxy = new InetSocketAddress(apiConfiguration.getProxyAddress(), apiConfiguration.getProxyPort());
+
+            httpClient = new OkHttpClient.Builder()
+                    .proxy(new Proxy(Proxy.Type.HTTP, proxy))
+                    .proxyAuthenticator((route, response) -> {
+                        String credential = Credentials.basic(apiConfiguration.getProxyUsername(), apiConfiguration.getProxyPassword());
+                        return response.request().newBuilder()
+                                .header("Proxy-Authorization", credential)
+                                .build();
+                    })
+                    .build();
+        }
+        else {
+            httpClient = new OkHttpClient();
+        }
+
+        final RequestBody body = RequestBody.create(FORM, tokenRequestBody);
+        final Request request = new Request.Builder()
+                .url(apiConfiguration.getTokenUrl())
+                .header("Accept", "application/json")
+                .post(body)
+                .build();
+
+        Response response = httpClient.newCall(request).execute();
+
+        if (response.code() != 200) {
+            throw new IOException(response.toString());
+        }
+
+        final String    content = response.body().string();
+        final ObjectMapper mapper = new ObjectMapper();
+
+        //  map json response
+        final Map bodyValues = mapper.readValue(content, Map.class);
+
+        if (!bodyValues.containsKey("access_token")) {
+            throw new IOException("missing access_token");
+        }
+
+        //  get access token
+        final String apiToken = (String)bodyValues.get("access_token");
+
+        ApiClient   apiClient = new ApiClient();
 
         if (apiConfiguration.getProxyAddress() != null) {
             apiClient.setHttpClient(httpClient);
         }
 
-        apiClient.addDefaultHeader("Authorization", "Bearer " + lusidToken.getAccessToken());
+        apiClient.addDefaultHeader("Authorization", "Bearer " + apiToken);
         apiClient.addDefaultHeader("X-LUSID-Application", apiConfiguration.getApplicationName());
         apiClient.setBasePath(apiConfiguration.getApiUrl());
 
-        return  apiClient;
-    }
-
-    private ApiConfiguration createApiConfiguration(String apiSecretsFilename){
-        try {
-            return new ApiConfigurationBuilder().build(apiSecretsFilename);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to build API Configuration. Ensure secrets files is properly setup.", e);
-        }
-    }
-
-    private OkHttpClient createHttpClient(ApiConfiguration apiConfiguration){
-        return new HttpClientBuilder().build(apiConfiguration);
+        return apiClient;
     }
 }
