d82f0b12d558442e5fba5bb48cc91076bc10b060
slacki123 <sebastian.lacki@finbourne.com>
2020-08-18 10:12:22 +0100

    SE-299 get annotations now returns a list rather than string

--- sdk/src/test/java/com/finbourne/features/FeatureExtractorTest.java
+++ sdk/src/test/java/com/finbourne/features/FeatureExtractorTest.java
@@ -4,16 +4,19 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.List;
 
 public class FeatureExtractorTest {
 
     @Test
     public void getAnnotationsAndWriteToFile() throws ClassNotFoundException, IOException, URISyntaxException {
         FeatureExtractor featureExtractor = new FeatureExtractor();
-        String annotations = featureExtractor.getAnnotations("com.finbourne.lusid.tutorials");
+        List<String> annotations = featureExtractor.getAnnotations("com.finbourne.lusid.tutorials");
+
+        String annotationsStr = String.join("\n", annotations);
 
         ParityFileWriter parityFileWriter = new ParityFileWriter();
-        parityFileWriter.writeToFile(annotations);
+        parityFileWriter.writeToFile(annotationsStr);
     }
 
 }
