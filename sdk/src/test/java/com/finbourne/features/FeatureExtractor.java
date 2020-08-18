c90e3ba831a0fbe9f9d1299b3fa00b97dfa4aa6b
slacki123 <sebastian.lacki@finbourne.com>
2020-08-18 09:52:42 +0100

    SE-299 renamed 'FeatureLookthrough' to 'FeatureExtractor'

--- sdk/src/test/java/com/finbourne/features/FeatureExtractor.java
+++ sdk/src/test/java/com/finbourne/features/FeatureExtractor.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 
-public class FeatureLookthrough {
+public class FeatureExtractor {
 
     String annotations;
 
