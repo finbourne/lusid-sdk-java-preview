372082753a05ea45c8bbf384b9441867f012f35e
slacki123 <sebastian.lacki@finbourne.com>
2020-09-29 14:48:25 +0100

    SE-299 code that will use cmd parser from mvn central

--- sdk/src/test/java/com/finbourne/features/Main.java
+++ sdk/src/test/java/com/finbourne/features/Main.java
@@ -7,42 +7,51 @@ import org.apache.commons.cli.*;
 
 
 public class Main {
-    public static void main(String args[]) throws ClassNotFoundException, IOException, NullFeatureValueException, DuplicateFeatureException, URISyntaxException {
+    public static void main(String args[]) throws ClassNotFoundException, IOException, NullFeatureValueException, DuplicateFeatureException, URISyntaxException, ParseException {
+        // Initialise options class for arg parser
         Options options = new Options();
 
-        Option pkg = new Option("p", "package", true, "package name, eg. 'com.finbourne.lusid.tutorials' (which is also the default option) ");
+        // Configure package parameter
+        Option pkg = new Option("p", "package", true, "package name, eg. 'com.finbourne.lusid.tutorials'");
         pkg.setRequired(true);
         options.addOption(pkg);
 
+        // Configure fully qualified filepath parameter
         Option fName = new Option("f", "filepath", true, "Fully qualified filepath name for the features file to be created. Eg. c/users/<some-dir>/<another-dir>/filename.txt");
         fName.setRequired(true);
         options.addOption(fName);
 
-        // this needs refactoring from this point on. Not sure if try/catch is needed
+        // Initialise command line parser
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
         CommandLine cmd = null;
 
 
         try {
+            // Attempt retrieving
             cmd = parser.parse(options, args);
         } catch (ParseException e) {
-            System.out.println(e.getMessage());
+            // If parameters are not passed correctly, that will throw a MissingOptionException
+            // and display the missing parameters in a pretty format
             formatter.printHelp("utility-name", options);
-
-            System.exit(1);
+            throw new ParseException(e.getMessage());
         }
 
+        // Retrieve the parameters passed in the cmd line
         String packageName = cmd.getOptionValue("package");
         String filepath = cmd.getOptionValue("filepath");
 
-        System.out.println("package name: " + packageName);
+        System.out.println(packageName);
 
+        // Initialise feature extractor and writer
         FeatureExtractor featureExtractor = new FeatureExtractor();
         FeatureFileWriter featureFileWriter = new FeatureFileWriter();
 
+        // Retrieve features from source code annotations
         List<String> annotations = featureExtractor.getAnnotations(packageName);
         String annotationsFromMethod = String.join("\n", annotations);
+
+        // Write features to file
         featureFileWriter.writeToFile(annotationsFromMethod, filepath);
         System.out.println("Done writing to filepath: " + filepath);
     }
