70aeec976b4129e0e6827ccf625f53eaa2abaa7f
Bogdan Lica <bogdan.lica@finbourne.com>
2021-06-11 12:49:45 +0100

    PLAT-345: tutorial updated

--- sdk/src/test/tutorial/java/com/finbourne/lusid/tutorials/ibor/Portfolios.java
+++ sdk/src/test/tutorial/java/com/finbourne/lusid/tutorials/ibor/Portfolios.java
@@ -166,7 +166,7 @@ public class Portfolios {
 
         //    Retrieve the transaction
         VersionedResourceListOfTransaction transactions = transactionPortfoliosApi.getTransactions(TutorialScope,
-                portfolioId, null, null, null, null, null);
+                portfolioId, null, null, null, null, null, null, null);
 
         assertEquals(1, transactions.getValues().size());
         assertEquals(transaction.getTransactionId(), transactions.getValues().get(0).getTransactionId());
@@ -239,7 +239,7 @@ public class Portfolios {
 
         //  get the trade
         VersionedResourceListOfTransaction transactions = transactionPortfoliosApi.getTransactions(TutorialScope,
-                portfolioId, null, null, null, null, null);
+                portfolioId, null, null, null, null, null, null, null);
 
         assertEquals(1, transactions.getValues().size());
         assertEquals(transaction.getTransactionId(), transactions.getValues().get(0).getTransactionId());
