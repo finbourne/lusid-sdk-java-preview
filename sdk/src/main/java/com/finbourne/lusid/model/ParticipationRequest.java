654406666b70cf798d183ac27afef4665487d91c
Concourse <concourse@finbourne.com>
2021-07-03 11:27:11 +0000

    Update SDK 0.11.3229

--- sdk/src/main/java/com/finbourne/lusid/model/ParticipationRequest.java
+++ sdk/src/main/java/com/finbourne/lusid/model/ParticipationRequest.java
@@ -141,10 +141,10 @@ public class ParticipationRequest {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
