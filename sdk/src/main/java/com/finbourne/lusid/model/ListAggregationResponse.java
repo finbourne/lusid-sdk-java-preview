ed38c11443e5ef0a2607594e73061b5ef1fb619d
Concourse <concourse@finbourne.com>
2021-07-09 07:11:58 +0000

    Update SDK 0.11.3250

--- sdk/src/main/java/com/finbourne/lusid/model/ListAggregationResponse.java
+++ sdk/src/main/java/com/finbourne/lusid/model/ListAggregationResponse.java
@@ -225,10 +225,10 @@ public class ListAggregationResponse {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
