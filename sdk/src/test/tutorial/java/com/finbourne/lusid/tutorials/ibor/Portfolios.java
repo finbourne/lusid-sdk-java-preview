69783f18cff674e1ec6f5f8e88aafbb7024e5670
fbn-sam <101395265+fbn-sam@users.noreply.github.com>
2022-05-10 10:58:16 +0100

    CTECH-1321: Updating GetTransactions calls with new showCancelledTransactions parameter (passing in null) (#123)

--- sdk/src/test/tutorial/java/com/finbourne/lusid/tutorials/ibor/Portfolios.java
+++ sdk/src/test/tutorial/java/com/finbourne/lusid/tutorials/ibor/Portfolios.java
@@ -167,7 +167,7 @@ public class Portfolios {
 
         //    Retrieve the transaction
         VersionedResourceListOfTransaction transactions = transactionPortfoliosApi.getTransactions(TutorialScope,
-                portfolioId, null, null, null, null, null, null, null);
+                portfolioId, null, null, null, null, null, null, null, null);
 
         assertEquals(1, transactions.getValues().size());
         assertEquals(transaction.getTransactionId(), transactions.getValues().get(0).getTransactionId());
@@ -240,7 +240,7 @@ public class Portfolios {
 
         //  get the trade
         VersionedResourceListOfTransaction transactions = transactionPortfoliosApi.getTransactions(TutorialScope,
-                portfolioId, null, null, null, null, null, null, null);
+                portfolioId, null, null, null, null, null, null, null, null);
 
         assertEquals(1, transactions.getValues().size());
         assertEquals(transaction.getTransactionId(), transactions.getValues().get(0).getTransactionId());
