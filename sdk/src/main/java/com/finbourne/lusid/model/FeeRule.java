61c5d9fb88653c349e4e24d20dc1cd562414d504
Concourse <concourse@finbourne.com>
2023-02-03 11:07:38 +0000

    Update SDK 0.11.5203 [rebase]

--- sdk/src/main/java/com/finbourne/lusid/model/FeeRule.java
+++ sdk/src/main/java/com/finbourne/lusid/model/FeeRule.java
@@ -432,10 +432,10 @@ public class FeeRule {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
