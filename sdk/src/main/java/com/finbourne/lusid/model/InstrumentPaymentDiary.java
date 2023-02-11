a2f4cf32d7bced669a0a1cade59c9c7bf9e7d4ac
Concourse <concourse@finbourne.com>
2023-02-11 20:15:30 +0000

    Update SDK 0.11.5226 [rebase]

--- sdk/src/main/java/com/finbourne/lusid/model/InstrumentPaymentDiary.java
+++ sdk/src/main/java/com/finbourne/lusid/model/InstrumentPaymentDiary.java
@@ -203,10 +203,10 @@ public class InstrumentPaymentDiary {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
