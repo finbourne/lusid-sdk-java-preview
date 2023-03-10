f6006193f8d83ec8a91b0a659635eaea4b4e38ed
Concourse <concourse@finbourne.com>
2023-03-10 20:32:50 +0000

    Update SDK 0.11.5312 [rebase]

--- sdk/src/main/java/com/finbourne/lusid/model/AborConfiguration.java
+++ sdk/src/main/java/com/finbourne/lusid/model/AborConfiguration.java
@@ -251,10 +251,10 @@ public class AborConfiguration {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
