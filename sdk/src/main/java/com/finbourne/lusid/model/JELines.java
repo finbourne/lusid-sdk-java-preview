519303666d7851688bba645ff36b6d03b523241e
Concourse <concourse@finbourne.com>
2023-03-20 21:48:08 +0000

    Update SDK 1.0.13 [rebase]

--- sdk/src/main/java/com/finbourne/lusid/model/JELines.java
+++ sdk/src/main/java/com/finbourne/lusid/model/JELines.java
@@ -444,10 +444,10 @@ public class JELines {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
