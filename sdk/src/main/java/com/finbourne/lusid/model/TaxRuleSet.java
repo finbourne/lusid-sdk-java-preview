11ca98bd1360c879fc0d533c81da6442c9f5cf19
Concourse <concourse@finbourne.com>
2023-04-25 21:38:59 +0000

    Update SDK 1.0.107 [rebase]

--- sdk/src/main/java/com/finbourne/lusid/model/TaxRuleSet.java
+++ sdk/src/main/java/com/finbourne/lusid/model/TaxRuleSet.java
@@ -199,10 +199,10 @@ public class TaxRuleSet {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
