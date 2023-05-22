154e40b0e4bba2408b5b519fa0278fb684f09078
Concourse <concourse@finbourne.com>
2023-05-22 15:18:33 +0000

    Update SDK 1.0.187 [rebase]

--- sdk/src/main/java/com/finbourne/lusid/model/ReconciliationRunBreak.java
+++ sdk/src/main/java/com/finbourne/lusid/model/ReconciliationRunBreak.java
@@ -187,10 +187,10 @@ public class ReconciliationRunBreak {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
