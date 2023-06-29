d24e789176aaa13cf9d7cdbd6d1ff94d41fd9455
Concourse <concourse@finbourne.com>
2023-06-29 18:26:24 +0000

    Update SDK 1.0.302 [rebase]

--- sdk/src/main/java/com/finbourne/lusid/model/SideDefinition.java
+++ sdk/src/main/java/com/finbourne/lusid/model/SideDefinition.java
@@ -213,10 +213,10 @@ public class SideDefinition {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
