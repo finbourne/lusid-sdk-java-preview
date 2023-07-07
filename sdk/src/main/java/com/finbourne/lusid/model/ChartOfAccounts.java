b30425887b49aaad556c06171e15150ea7f4e653
Concourse <concourse@finbourne.com>
2023-07-07 17:19:45 +0000

    Update SDK 1.0.319 [rebase]

--- sdk/src/main/java/com/finbourne/lusid/model/ChartOfAccounts.java
+++ sdk/src/main/java/com/finbourne/lusid/model/ChartOfAccounts.java
@@ -205,10 +205,10 @@ public class ChartOfAccounts {
   }
 
    /**
-   * Collection of links.
+   * Get links
    * @return links
   **/
-  @ApiModelProperty(value = "Collection of links.")
+  @ApiModelProperty(value = "")
   public List<Link> getLinks() {
     return links;
   }
